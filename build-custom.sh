46505f947cf4e575f0a24c634198d8e4b9326a7f
Nick Douma <n.douma@nekoconeko.nl>
2016-07-25 22:11:44 +0200

    Add new custom pack image based on Alpine.
    Should provide the same functionality as the normal image, at one third of the size.

--- build-custom.sh
+++ build-custom.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source tools/check-docker.sh
-(cd custom && $DOCKER build -t minecraft:custom .)
+(
+    cd custom &&
+    $DOCKER build -t lordgaav/minecraft-docker:custom -f Dockerfile .;
+    $DOCKER build -t lordgaav/minecraft-docker:custom-alpine -f Dockerfile-alpine .
+)
