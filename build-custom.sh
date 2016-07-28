e98b52e20f313b773d1951a711f6800d06c21b8d
Nick Douma <n.douma@nekoconeko.nl>
2016-07-28 20:14:42 +0200

    Replace Ubuntu custom Minecraft docker with Alpine version

--- build-custom.sh
+++ build-custom.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 source tools/check-docker.sh
-(
-    cd custom &&
-    $DOCKER build -t lordgaav/minecraft-docker:custom -f Dockerfile .;
-    $DOCKER build -t lordgaav/minecraft-docker:custom-alpine -f Dockerfile-alpine .
-)
+(cd custom && $DOCKER build -t lordgaav/minecraft-docker:custom -f Dockerfile .)
